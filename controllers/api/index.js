b280f731fde2e1b07143fc56f4599e5cbc94afa8
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2023-04-23 12:26:57 -0400

    fixed bug

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
+const dashboardRoutes = require('../homeRoutes');
 
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
